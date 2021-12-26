965a5d42258204fc6f32402f3bd39f5323516f6c
Alexandre <alex.chevreaud@icloud.com>
2021-12-26 10:36:48 +0100

    ajout du #corps et début login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {accueilComponent} from "./Component/Accueil/accueil.component";
 import {EntrepriseComponent} from "./Component/entreprise/entreprise.component";
+import {LogInComponent} from "./Component/log-in/log-in.component";
 
 const routes: Routes = [
   {path: 'accueil', component: accueilComponent},
   {path: 'entreprise', component: EntrepriseComponent},
-  {path: 'login', component: EntrepriseComponent},
+  {path: 'login', component: LogInComponent},
   {path: '', redirectTo: '/login', pathMatch: 'full'}
 ];
 

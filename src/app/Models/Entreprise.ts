effffb40951725dd7bac1aa9c9faa3799c703ec2
Antoine BIGEARD <antoine.bigeard.tpe@gmail.com>
2021-12-31 17:07:32 +0100

    Post works

--- src/app/Models/Entreprise.ts
+++ src/app/Models/Entreprise.ts
@@ -1,18 +1,19 @@
 export interface Entreprise {
 
-  numEntreprise : number ;
-  raisonSociale : string ;
-  nomContact :string ;
-  Nom_Resp : string ;
-  rueEntreprise :string ;
-  cpEntreprise: number ;
-  villeEntreprise: string ;
-  telEntreprise: string ;
-  faxEntreprise: string ;
-  Email: string ;
-  Observation: string ;
-  siteEntreprise: string ;
-  Niveau: string ;
+  numEntreprise: number;
+  raisonSociale: string;
+  nomContact: string;
+  nomResp: string;
+  rueEntreprise: string;
+  cpEntreprise: number;
+  villeEntreprise: string;
+  telEntreprise: string;
+  faxEntreprise: string;
+  Email: string;
+  observation: string;
+  siteEntreprise: string;
+  niveau: string;
+  enActivite: number;
 
 
 }

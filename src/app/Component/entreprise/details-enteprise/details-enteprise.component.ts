34aed2ab16938f4d5e09288f38916bf2ad3392bb
Antoine BIGEARD <antoine.bigeard.tpe@gmail.com>
2022-01-07 21:44:39 +0100

    Fin affichage

--- src/app/Component/entreprise/details-enteprise/details-enteprise.component.ts
+++ src/app/Component/entreprise/details-enteprise/details-enteprise.component.ts
@@ -4,6 +4,7 @@ import {LogInService} from "../../../Services/LogInService";
 import {EntrepriseService} from "../../../Services/EntrepriseService";
 import {Entreprise} from "../../../Models/Entreprise";
 import {Specialite} from "../../../Models/Specialite";
+import {SpecialiteService} from "../../../Services/SpecialiteService";
 
 @Component({
   selector: 'app-details-enteprise',
@@ -14,14 +15,15 @@ export class DetailsEntepriseComponent implements OnInit {
 
   entreprise = <Entreprise>{};
   //TODO check ca
-
-  specialites : Specialite[] = [];
+  specEntreprise: string = "";
+  specialites: Specialite[] = [];
 
 
   constructor(private router: Router,
               private logInService: LogInService,
               private entrepriseService: EntrepriseService,
-              private route: ActivatedRoute
+              private specialiteService: SpecialiteService,
+              private route: ActivatedRoute,
   ) {
 
   }
@@ -29,13 +31,15 @@ export class DetailsEntepriseComponent implements OnInit {
   ngOnInit(): void {
     if (this.route.snapshot.queryParams["numEntreprise"]) {
       let id: number = <number>this.route.snapshot.queryParams["numEntreprise"];
+      this.specialiteService.getAllSpecialite().subscribe((value => {
+        this.specialites = value;
+      }));
       this.entrepriseService.getEntrepriseAndSpecById(id).subscribe(value => {
         console.log(value);
         this.entreprise = value.entreprise;
         let specEntreprises = value.specEntreprises;
-        value.specEntreprises.forEach((item)=>
-        {
-
+        value.specEntreprises.forEach((item) => {
+          this.specEntreprise += this.specialites.find(x => x.numSpec == item.numSpec)?.libelle + " " ?? "";
         });
       });
     }
